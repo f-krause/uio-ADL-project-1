165c002fabb580df38857f8b96397fcd54093f83
User3574 <69751521+User3574@users.noreply.github.com>
2023-09-05 22:36:52 +0200

    ADD: LoRA, Exercises

---
+++ src/lora.py
@@ -0,0 +1,68 @@
+import torch
+from torch import nn
+
+
+class LoRALayer(nn.Module):
+    def __init__(
+        self,
+        qkv: nn.Module,
+        Aq: nn.Module,
+        Bq: nn.Module,
+        Av: nn.Module,
+        Bv: nn.Module,
+    ):
+        super().__init__()
+        self.qkv = qkv
+        self.k = qkv.in_features
+        self.Aq, self.Av = Aq, Av
+        self.Bq, self.Bv = Bq, Bv
+
+    def forward(self, x):
+        # W + BAx has to be done for Q,V,K since it's in one big matrix Wqkv
+        # return W + BAx
+        W = self.qkv(x)  # B, N, 3*k
+        W[:, :, :self.k] += self.Bq(self.Aq(x))   # For Q
+        W[:, :, -self.k:] += self.Bv(self.Av(x))  # For V
+        return W
+
+
+class LoRATransformer(nn.Module):
+    def __init__(self, transformer, r):
+        super().__init__()
+
+        # Freeze
+        for param in transformer.parameters():
+            param.requires_grad = False
+
+        # Iterate over transformer
+        for layer, block in enumerate(transformer.blocks):
+            """
+            From the paper:
+            h = W0x + ∆W x = W0x + BAx
+            
+            Weight matrix W0 ∈ R^d×k, we constrain its update by representing the latter with a low-rank de-
+            composition W0 + ∆W = W0 + BA, where B ∈ R^d×r , A ∈ R^r×k, and the rank r = min(d, k)
+            
+            With r = 4 and only the query (Wq) and value (Wv) projection matrices being adapted, the checkpoint
+            size is reduced by roughly 10,000× (from 350GB to 35MB).
+            """
+            print(block)
+            k = block.attn.qkv.in_features
+
+            # Use LoRALayer
+            block.attn.qkv = LoRALayer(
+                # Wqkv
+                block.attn.qkv,
+                # Wq [W = d x k, B = d x r, A = r x k]
+                nn.Linear(k, r, bias=False),
+                nn.Linear(r, k, bias=False),
+                # Wv
+                nn.Linear(k, r, bias=False),
+                nn.Linear(r, k, bias=False),
+            )
+
+        self.lora = transformer
+        self.lora.reset_classifier(num_classes=10)
+
+    def forward(self, x):
+        return self.lora(x)
--- src/main.py
+++ src/main.py
@@ -8,9 +8,14 @@ import time
 from tqdm import tqdm
 from torchvision import transforms
 from timm import optim
+from lora import LoRATransformer
 
 
-def get_data_loader(path: str, transforms, shuffle: bool, batch_size: int) -> torch.utils.data.DataLoader:
+# Define device
+device = 'cuda'
+
+
+def get_dataloader(path: str, transforms, shuffle: bool, batch_size: int):
     root_path = path
     transforms = transforms
 
@@ -30,31 +35,48 @@ def get_data_loader(path: str, transforms, shuffle: bool, batch_size: int) -> to
 
 
 def train(model, dataloader, epochs, optimizer, loss_fnc):
+    model.train()
     for epoch in range(epochs):
         total_loss = 0
         for batch in tqdm(dataloader):
             optimizer.zero_grad()
+
+            # Load data onto device
             inputs, targets = batch
+            inputs = inputs.to(device)
+            targets = targets.to(device)
+
+            # Predictions
             outputs = model(inputs)
             loss = loss_fnc(outputs, targets)
-            total_loss += loss.item()
+
+            # Optimize
             loss.backward()
             optimizer.step()
+            total_loss += loss.item()
+
         print(f'Epoch: {epoch}, loss: {total_loss}')
 
 
-def eval(model, dataloader):
+def eval(model, dataloader, loss_fnc):
     model.eval()
     total_loss = 0
     pred_targets = np.array([])
     pred_outputs = np.array([])
+
     for batch in tqdm(dataloader):
+        # Load data onto device
         inputs, targets = batch
+        inputs = inputs.to(device)
+        targets = targets.to(device)
+
+        # Predictions
         outputs = model(inputs)
-        pred_targets = np.hstack([pred_targets, targets.numpy()])
-        pred_outputs = np.hstack([pred_outputs, np.argmax(outputs.detach().numpy(), axis=1)])
+        pred_targets = np.hstack([pred_targets, targets.cpu().numpy()])
+        pred_outputs = np.hstack([pred_outputs, np.argmax(outputs.cpu().detach().numpy(), axis=1)])
         loss = loss_fnc(outputs, targets)
         total_loss += loss.item()
+
     print(f'Loss: {total_loss}')
     print(f'Accuracy: {np.mean((pred_targets == pred_outputs))}')
 
@@ -67,37 +89,48 @@ def measure(fnc):
 
 
 if __name__ == '__main__':
+    # Define dataloaders
     INPUT_SHAPE = (224, 224, 3)
-    train_loader = get_data_loader("../imagewoof/train",
-                                   transforms.Compose([
-                                       transforms.ToTensor(),
-                                       transforms.Resize(INPUT_SHAPE[:2]),
-                                       transforms.Normalize([0, 0, 0], [255, 255, 255])
-                                   ]),
-                                   False,
-                                   4)
-    test_loader = get_data_loader("../imagewoof/train",
-                                  transforms.Compose([
-                                      transforms.ToTensor(),
-                                      transforms.Resize(INPUT_SHAPE[:2]),
-                                      transforms.Normalize([0, 0, 0], [255, 255, 255])
-                                  ]),
-                                  True,
-                                  4)
+    train_loader = get_dataloader(
+        "../imagewoof/train",
+        transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Resize(INPUT_SHAPE[:2]),
+        ]),
+        False,
+        64
+    )
+    test_loader = get_dataloader(
+        "../imagewoof/train",
+        transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Resize(INPUT_SHAPE[:2]),
+        ]),
+        True,
+        64
+    )
 
-    model = timm.create_model('vit_tiny_patch16_224', num_classes=10, pretrained=True)
-    optimizer = timm.optim.AdamP(model.parameters(), lr=0.01)
+    # Exercise 1:
+    # # Define model for fine-tuning
+    # model = timm.create_model('vit_tiny_patch16_224', pretrained=True)
+    # for param in model.parameters():
+    #     param.requires_grad = False
+    # model.head = torch.nn.Linear(model.head.in_features, 10)
+    # model = model.to(device)
+    # optimizer = timm.optim.AdamW(model.parameters())
+    # loss_fnc = torch.nn.CrossEntropyLoss()
+    #
+    # measure(lambda: eval(model, test_loader, loss_fnc))
+    # measure(lambda: train(model, train_loader, 5, optimizer, loss_fnc))
+    # measure(lambda: eval(model, test_loader, loss_fnc))
+
+    # Exercise 2:
+    r = 10
+    model = LoRATransformer(timm.create_model('vit_tiny_patch16_224', pretrained=True), r)
+    model = model.to(device)
+    optimizer = timm.optim.AdamW(model.parameters())
     loss_fnc = torch.nn.CrossEntropyLoss()
 
-    # train(
-    #     model,
-    #     train_loader,
-    #     5,
-    #     optimizer,
-    #     loss_fnc
-    # )
+    measure(lambda: train(model, train_loader, 10, optimizer, loss_fnc))
+    measure(lambda: eval(model, test_loader, loss_fnc))
 
-    measure(lambda: eval(
-        model,
-        test_loader
-    ))
