ef6185fafea9696b6681ed29a61afd2d42f0243c
Felix Krause <felix_krause1999@yahoo.de>
2023-09-06 22:50:09 +0200

    adds print statements

--- src/main.py
+++ src/main.py
@@ -1,19 +1,9 @@
 import argparse
-import torch
-import numpy as np
-import timm
-import time
+from datetime import datetime
 import os
-from tqdm import tqdm
-from torchvision import transforms
-from timm import optim
-from lora import LoRATransformer
 
 from get_data import get_dataloaders_educloud, get_dataloaders_local
 from train import run_full_tuning, run_lora_tuning
-
-
-
 
 
 parser = argparse.ArgumentParser(description="Specify parameters and tasks of KNN based genre estimation")
@@ -29,17 +19,21 @@ def main():
 
     if os.path.isdir("/projects/ec232/data/"):
         # Load data from educloud server if path exists
+        print(datetime.now(), "Loading data from educloud server")
         train_loader, test_loader = get_dataloaders_educloud(BATCH_SIZE, INPUT_SHAPE)
     else:
         # Otherwise load local data from "imagewoof" directory
+        print(datetime.now(), "Loading data from local path")
         train_loader, test_loader = get_dataloaders_local(BATCH_SIZE, INPUT_SHAPE)
 
     if not args.lora:
         # Exercise 1 - full fine-tuning
-        run_full_tuning(train_loader, test_loader, args.save)
+        print(datetime.now(), f"Starting full fine-tuning with {args.epochs} epochs")
+        run_full_tuning(train_loader, test_loader, args.epochs, args.save)
     else:
         # Exercise 2 - LoRA approach
-        run_lora_tuning(train_loader, test_loader, args.save)
+        print(datetime.now(), f"Starting LoRA training with {args.epochs} epochs")
+        run_lora_tuning(train_loader, test_loader, args.epochs, args.save)
 
 
 if __name__ == '__main__':
